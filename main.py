98e4ed82c39f9c82e003833b624d1a4dcb35cf70
samue1eun <samuel.eun.park@gmail.com>
2025-03-15 20:47:26 +0100

    fixed the @app.get

--- main.py
+++ main.py
@@ -23,3 +23,25 @@ books = [
         "language": "English",
     }
 ]
+
+
+@app.get('/books')
+async def get_all_books():
+    return books
+
+
+@app.post('/books')
+async def create_a_book() -> dict:
+    pass
+
+@app.get('/book/{book_id}')
+async def get_book(book_id:int) -> dict:
+    pass
+
+@app.get('/book/{book_id}')
+async def update_book(book_id:int) -> dict:
+    pass
+
+@app.get('/book/{book_id}')
+async def delete_book(book_id:int) -> dict:
+    pass
