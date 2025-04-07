a80bfcbb9ee116abbeecbb0abcdd279fc9bbe20c
samue1eun <samuel.eun.park@gmail.com>
2025-04-07 21:06:39 +0200

    CRUDing continued

--- src/books/schemas.py
+++ src/books/schemas.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel
+from datetime import datetime
 import uuid
 
 class Book(BaseModel):
@@ -9,8 +10,16 @@ class Book(BaseModel):
         published_date: str
         page_count: int
         language: str
+        created_at: datetime
+        update_at: datetime
 
-
+class BookCreateModel(BaseModel):
+        title: str
+        author: str
+        publisher: str
+        published_date: str
+        page_count: int
+        language: str
 class BookUpdateModel(BaseModel):
         title: str
         author: str
