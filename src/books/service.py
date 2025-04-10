4c64d276af65ac2a5add125b6b2e00df64f1a4e9
samue1eun <samuel.eun.park@gmail.com>
2025-04-10 22:44:33 +0200

    Need to troubleshoot the 404 error

--- src/books/service.py
+++ src/books/service.py
@@ -12,7 +12,8 @@ class BookService:
     async def get_book(self, book_uid: str, session:AsyncSession):
         statement = select(Book).where(Book.uid == book_uid)
         result = await session.exec(statement)
-        return result.first()
+        book = result.first()
+        return book if book is not None else None
 
     async def create_book(self, book_data:BookCreateModel, session:AsyncSession):
         book_data_dict = book_data.model_dump()
@@ -25,9 +26,31 @@ class BookService:
         await session.commit()
         return new_book
 
-    async def update_book(self, book_uid: str, book_data:BookUpdateModel, session:AsyncSession):
-        pass
+    async def update_book(self, book_uid: str, update_data:BookUpdateModel, session:AsyncSession):
+        book_to_update = self.get_book(book_uid, session)
+        if book_to_update is not None:
+            
+            update_data_dict = update_data.model_dump()
+
+            for k, v in update_data_dict.items():
+                setattr(book_to_update, k, v)
+            
+            await session.commit()
+
+            return book_to_update
+        else:
+            return None
+
 
     async def delete_book(self, book_uid:str, session:AsyncSession):
-        pass
+        book_to_delete = self.get_book(book_uid, session)
+
+        if book_to_delete is not None:
+            await session.delete(book_to_delete)
+            await session.commit()
+            return {}
+        
+        else:
+            return None
+
 
--- src/db/main.py
+++ src/db/main.py
@@ -1,6 +1,8 @@
 from sqlmodel import create_engine, text, SQLModel
 from sqlalchemy.ext.asyncio import AsyncEngine
 from src.config import Config
+from sqlmodel.ext.asyncio.session import AsyncSession
+from sqlalchemy.orm import sessionmaker
 
 engine = AsyncEngine(
     create_engine(
@@ -12,3 +14,13 @@ async def init_db():
     async with engine.begin() as conn:
         from src.books.models import Book
         await conn.run_sync(SQLModel.metadata.create_all)
+
+async def get_session() -> AsyncSession:
+    Session = sessionmaker(
+        bind=async_engine,
+        class_=AsyncSession,
+        expire_on_commit=False,
+    )
+    
+    async with Session() as session:
+        yield session 
